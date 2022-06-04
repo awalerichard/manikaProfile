de935530b044b89a2ee71a4a7f43d7d172ed67f2
Richard Awale <richardawale182@gmail.com>
2022-06-04 13:20:37 +0300

    navigation bar

--- src/screens/Homepage/index.js
+++ src/screens/Homepage/index.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Banner from "../../component/Banner";
+import Menu from "../../component/Menu";
 
 const Homepage = () => {
-  return <div>Hello testing.</div>;
+  return (
+    <div>
+      <h1> Hello world</h1>
+      <Menu />
+    </div>
+  );
 };
 
 export default Homepage;
