c5250890e29d6c8256e97492fd423365bbff3e8f
Richard Awale <richardawale182@gmail.com>
2022-06-04 12:44:51 +0300

    initial commit react app created

---
+++ src/navigation/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+//import { Route, Routes } from "react-router-dom";
+
+import Homepage from "../screens/Homepage";
+
+const MainNavigation = () => {
+  return <Homepage />;
+};
+
+export default MainNavigation;
